3942f056ef02532149636dd6268bf573e0b4c36e
IceMaD <icemad1109@gmail.com>
2021-03-24 19:18:05 +0100

    Add user filter

--- assets/app.js
+++ assets/app.js
@@ -14,11 +14,15 @@ class GameRow {
   _tr
   name
   tags
+  users
 
   constructor(tr) {
     this._tr = tr
     this.tags = JSON.parse(tr.getAttribute('tags'))
     this.name = tr.getAttribute('game')
+    this.users = [...tr.querySelectorAll('td[user]')]
+      .filter(td => 'true' === td.getAttribute('has'))
+      .map(td => parseInt(td.getAttribute('user'), 10))
   }
 
   hide() {
@@ -33,6 +37,7 @@ class GameRow {
 class Filters {
   _filters = {
     selectedTags: [],
+    userIds: [],
     gameName: ''
   }
 
@@ -58,13 +63,31 @@ class Filters {
     this.filter()
   }
 
+  toggleUser(userId) {
+    if (this.hasUser(userId)) {
+      this._filters.userIds = this._filters.userIds.filter(id => id !== userId)
+    } else {
+      this._filters.userIds = [...this._filters.userIds, userId]
+    }
+
+    this.filter()
+  }
+
+  hasUser(userId) {
+    return this._filters.userIds.includes(userId)
+  }
+
   filter() {
     this._gameRows.forEach(gameRow => {
-      if (-1 === gameRow.name.toLowerCase().indexOf(this._filters.gameName.toLowerCase())) {
+      if (!gameRow.name.toLowerCase().includes(this._filters.gameName.toLowerCase())) {
         return gameRow.hide()
       }
 
-      if (this._filters.selectedTags.length > 0 && !gameRow.tags.filter(value => this._filters.selectedTags.includes(value)).length > 0) {
+      if (0 < this._filters.selectedTags.length && 0 === gameRow.tags.filter(value => this._filters.selectedTags.includes(value)).length) {
+        return gameRow.hide()
+      }
+
+      if (0 < this._filters.userIds.length && 0 === gameRow.users.filter(value => this._filters.userIds.includes(value)).length) {
         return gameRow.hide()
       }
 
@@ -86,4 +109,18 @@ if (allGamesTable) {
 
   gameInput.addEventListener('keyup', event => filters.setGameName(event.target.value))
   gameInput.addEventListener('blur', event => filters.setGameName(event.target.value))
+
+  allGamesTable.querySelectorAll('thead td[user]').forEach(td => {
+    const userId = parseInt(td.getAttribute('user'), 10)
+
+    td.addEventListener('click', () => {
+      filters.toggleUser(userId)
+
+      if (filters.hasUser(userId)) {
+        td.classList.add('selected')
+      } else {
+        td.classList.remove('selected')
+      }
+    })
+  })
 }
