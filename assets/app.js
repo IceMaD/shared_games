651e34b1722167aea92fc12c9d529dd3d9cb1b3f
IceMaD <icemad1109@gmail.com>
2021-03-25 21:34:24 +0100

    Fix issue while adding game

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,13 @@
 import './styles/app.scss'
+import {tagInputManager} from './scripts/TagInputManager'
 
 let route = document.querySelector('body').getAttribute('route')
 let scriptMap = {
   home: 'Home',
   tags: 'Tags'
 }
+
+tagInputManager.init()
 
 if (scriptMap[route]) {
   import(`./scripts/Page/${scriptMap[route]}`)
